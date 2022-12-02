34245876da8d416b04a8742eb881b81c9d56b006
SalsaGal <fielddashos@gmail.com>
2022-12-02 19:12:00 +1100

    Initial commit

---
+++ src/1.hs
@@ -0,0 +1,9 @@
+main :: IO ()
+main = do
+    input <- readFile "input/1.txt"
+    let input_split = lines input
+    let sums = foldl handle_input [0] input_split
+    print sums
+
+handle_input :: [Int] -> String -> [Int]
+handle_input acc line = if line == "" then acc ++ [0] else (init acc) ++ [(last acc) + read line]
