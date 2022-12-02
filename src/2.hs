425e16cea1f39270b5f6fbe3ae609273b78675c5
SalsaGal <fielddashos@gmail.com>
2022-12-02 20:23:30 +1100

    Broken day 2

---
+++ src/2.hs
@@ -0,0 +1,39 @@
+main :: IO ()
+main = do
+    strategy <- readFile "input/2.txt"
+    --let moves = map move_to_line (lines strategy)
+    let moves = map move_to_line (lines "A Y\nB X\nC Z")
+    let scores = map score moves
+    print (sum scores)
+
+move_to_line :: String -> [String]
+move_to_line move = [
+        (
+            case move !! 0 of
+                'A' -> "R"
+                'B' -> "P"
+                'C' -> "S"
+                _ -> "Bruh"
+        ),
+        (
+            case move !! 2 of
+                'X' -> "R"
+                'Y' -> "P"
+                'Z' -> "S"
+                _ -> "Bruh"
+        )
+    ]
+
+score :: [String] -> Int
+score moves = 1 + move_index (last moves) + round_score (map move_index moves)
+
+round_score :: [Int] -> Int
+round_score [a, b] =
+    if (a + 1 `mod` 3) == b then 6
+    else if a == b then 3
+    else 0
+
+move_index :: String -> Int
+move_index "R" = 0
+move_index "P" = 1
+move_index "S" = 2
