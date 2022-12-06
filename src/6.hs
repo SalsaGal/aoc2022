8d10d1eaaf03e3f33d1e7b173a1783aa9e7b6d90
SalsaGal <fielddashos@gmail.com>
2022-12-06 18:56:07 +1100

    Solve 6

--- src/6.hs
+++ src/6.hs
@@ -1,9 +1,11 @@
 main :: IO ()
 main = do
-    input <- readFile "input/6example.txt"
+    input <- readFile "input/6.txt"
     let transmissions = lines input
-    print (unique "foab")
-    print (unique "boab")
+    print (map get_start transmissions)
+
+get_start :: String -> Int
+get_start message = fst (head (filter (\(_,x) -> unique x) (zip [0..] (window 4 message)))) + 4
 
 unique :: String -> Bool
 unique input = not (any (1<) (map (\x -> length (filter (x==) input)) input))
