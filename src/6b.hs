11a2cd78931c7952592027fd76d86a50125863c7
SalsaGal <fielddashos@gmail.com>
2022-12-06 18:58:10 +1100

    Solve 6b

---
+++ src/6b.hs
@@ -0,0 +1,15 @@
+main :: IO ()
+main = do
+    input <- readFile "input/6.txt"
+    let transmissions = lines input
+    print (map get_start transmissions)
+
+get_start :: String -> Int
+get_start message = fst (head (filter (\(_,x) -> unique x) (zip [0..] (window 14 message)))) + 14
+
+unique :: String -> Bool
+unique input = not (any (1<) (map (\x -> length (filter (x==) input)) input))
+
+window :: Int -> String -> [String]
+window _ "" = []
+window num list = [take num list] ++ window num (tail list)
